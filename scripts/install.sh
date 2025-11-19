bad19d31d250077227194822975e1c79734a1a5b
Yamac Ay <yamacerenay2001@gmail.com>
2025-11-19 13:52:04 +0100

    slurm commands

---
+++ scripts/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+DONEFILE="/tmp/install_done_${SLURM_JOBID}"
+if [[ $SLURM_LOCALID == 0 ]]; then
+  apt update
+  apt install -y [...]
+  apt clean
+  python -m pip install --upgrade pip
+  if [ -f requirements.txt ]; then
+    pip install -r requirements.txt
+  else
+    echo "requirements.txt not found, skipping pip install."
+    exit 1
+  fi
+  touch "${DONEFILE}"
+else
+  while [[ ! -f "${DONEFILE}" ]]; do sleep 1; done
+fi
+"$@"
